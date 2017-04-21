04a7a5e1e9bb57bf7d0c96b518b588a084e26ff8
Christopher Brown <io@henrian.com>
2017-04-21 12:07:03 -0500

    Remove emacs config

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-((nil
-  (fill-column . 80)
-  (whitespace-line-column . nil))
- (clojure-mode
-  (clojure-test-ns-segment-position . 1)
-  (eval ignore-errors
-        (require 'whitespace)
-        (whitespace-mode 0)
-        (whitespace-mode 1))))
